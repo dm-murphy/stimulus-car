b115cfaabd90fdd191ea5e6886f353e6cf2dfc54
Daniel Murphy <danielmichaelmurphy@gmail.com>
2022-05-17 15:57:17 -0400

    Add another element to toggle for hide/show with #hide

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -1,13 +1,14 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "source", "hideable" ]
+  static targets = [ "source", "hideable", "showable" ]
 
   copy() {
     navigator.clipboard.writeText(this.sourceTarget.value)
   }
 
   hide() {
-    this.hideableTarget.hidden = "true"
+    this.hideableTarget.hidden = true
+    this.showableTarget.hidden = false
   }
 }
